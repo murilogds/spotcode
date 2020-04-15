242443918f544a67b06de4289cb3ffe6b8c89813
Murilo Gomes <murilo.goomes@hotmail.com>
2020-04-15 11:39:17 -0300

    Criando endpoints de discovery e de busca

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
   
+  namespace :api, defaults: { format: :json} do
+    namespace :v1 do
+      resources :dashboard, only: :index
+      resources :categories, only: :index
+      resources :search, only: :index
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
